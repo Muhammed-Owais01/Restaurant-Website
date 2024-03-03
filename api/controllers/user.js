f4144d493cb38e2d58fe560eb750405a260ebc45
fasihh <fhk.tcs@gmail.com>
2024-03-03 15:11:55 +0500

    fixed username/email error

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -5,101 +5,73 @@ const bcrypt = require('bcrypt');
 
 exports.user_signup = (req, res, next) => {
     User.find({ username: req.body.username })
-        .exec()
-        .then(user => {
-            if (user.length >= 1)
-            {
-                res.status(409).json({
-                    message: "User Already Exists"
-                });
-            }
-            else {
-                bcrypt.hash(req.body.password, 10, (err, hash) => {
-                    if (err)
-                    {
-                        return res.status(500).json({
-                            error: err
-                        });
-                    }
-                    else {
-                        const user = new User({
-                            _id: new mongoose.Types.ObjectId(),
-                            username: req.body.username,
-                            password: hash
-                        });
-                        user.save()
-                            .then(result => {
-                                console.log(result);
-                                res.status(201).json({
-                                    message: "User Created"
-                                });
-                            })
-                            .catch(err => {
-                                res.status(500).json({
-                                    error: err
-                                });
-                            });
-                    }
-                })
-            }
+    .exec()
+    .then(user => {
+        console.log(user);
+        if (user.length >= 1) return res.status(409).json({ message: "User Already Exists" });
+        
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+            if (err) return res.status(500).json({ error: err });
+
+            const user = new User({
+                _id: new mongoose.Types.ObjectId(),
+                username: req.body.username,
+                cart: [],
+                password: hash
+            });
+            user.save()
+            .then(result => {
+                console.log(result);
+                res.status(201).json({ message: "User Created" });
+            })
+            .catch(err => res.status(500).json({ error: err }));
         });
+    });
 };
 
 exports.user_login = (req, res, next) => {
-    User.find({email: req.body.email})
-        .exec()
-        .then(users => {
-            if (users.length < 1)
+    User.find({username: req.body.username})
+    .exec()
+    .then(users => {
+        if (users.length < 1)
+        {
+            console.log("User Not Found");
+            return res.status(401).json({ message: "Authorization Failed" });
+        }
+        bcrypt.compare(req.body.password, users[0].password, (err, result) => {
+            if (err)
             {
-                console.log("User Not Found");
-                return res.status(401).json({
-                    message: "Authorization Failed"
+                console.log("PASS NOT MATCH");
+                return res.status(401).json({ message: "Authorization Failed" });
+            }
+            if (result)
+            {
+                const token = jwt.sign(
+                {
+                    username: users[0].username,
+                    userId: users[0]._id
+                },
+                process.env.JWT_KEY,
+                // {
+                //     expiresIn: "4h"
+                // }
+                );
+                res.status(200).json({
+                    message: "User logged in",
+                    token: `Bearer ${token}`
                 });
             }
-            bcrypt.compare(req.body.password, users[0].password, (err, result) => {
-                if (err)
-                {
-                    console.log("PASS NOT MATCH");
-                    return res.status(401).json({
-                        message: "Authorization Failed"
-                    });
-                }
-                if (result)
-                {
-                    const token = jwt.sign(
-                    {
-                        username: users[0].username,
-                        userId: users[0]._id
-                    },
-                    process.env.JWT_KEY,
-                    // {
-                    //     expiresIn: "4h"
-                    // }
-                    );
-                    res.status(200).json({
-                        message: "User logged in",
-                        token: `Bearer ${token}`
-                    });
-                }
-            })
-        })
-        .catch(err => {
-            res.status(500).json({
-                error: err
-            })
-        })
+        });
+    })
+    .catch(err => res.status(500).json({ error: err }));
 };
 
 exports.delete_user = (req, res, next)=> {
     User.deleteOne({_id: req.params.userId})
-        .exec()
-        .then(user => {
-            console.log(result);
-            res.status(200).json({
-                message: "User Deleted"
-            });
-        })
-        .catch(err => {
-            res.status(500).json({error: err});
-        });
+    .exec()
+    .then(user => {
+        console.log(result);
+        res.status(200).json({ message: "User Deleted" });
+    })
+    .catch(err => res.status(500).json({error: err}));
 }
